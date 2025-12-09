93a2adf313b48a1231ba7c2371cae9c6ba5acf28
Praveen <praveenc1932@gmail.com>
2025-12-09 04:25:46 +0000

    Update praveen.js

--- praveen.js
+++ praveen.js
@@ -1,60 +1,57 @@
-/*=============== SHOW MENU ===============*/
+/*==================== SHOW MENU ====================*/
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
           nav = document.getElementById(navId);
 
     if (toggle && nav) {
-        toggle.addEventListener("click", () => {
-            nav.classList.toggle("show");
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');
         });
     }
 };
-showMenu("nav-toggle", "nav-menu");
+showMenu('nav-toggle', 'nav-menu');
 
-/*=============== REMOVE MENU (MOBILE) ===============*/
-const navLink = document.querySelectorAll(".nav__link");
+/*==================== REMOVE MENU ON MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
-    const navMenu = document.getElementById("nav-menu");
-    navMenu.classList.remove("show");
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
 }
-navLink.forEach(n => n.addEventListener("click", linkAction));
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*=============== ACTIVE SECTIONS ===============*/
-const sections = document.querySelectorAll("section[id]");
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]');
 
-function scrollActive() {
-    const scrollY = window.pageYOffset;
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
 
     sections.forEach(current => {
-        const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 60,
-              sectionId = current.getAttribute("id"),
-              link = document.querySelector(".nav__menu a[href*=" + sectionId + "]");
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute('id');
+        const sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
 
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            link.classList.add("active-link");
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link');
         } else {
-            link.classList.remove("active-link");
+            sectionsClass.classList.remove('active-link');
         }
     });
-}
-window.addEventListener("scroll", scrollActive);
+};
+window.addEventListener('scroll', scrollActive);
 
-/*=============== SCROLL REVEAL ANIMATIONS ===============*/
+/*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
-    origin: "top",
-    distance: "60px",
-    duration: 1500,
-    delay: 150
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+    // reset: true
 });
 
-sr.reveal(".home__data, .section-title", {});
-sr.reveal(".home__img", { delay: 300 });
-sr.reveal(".home__social-icon", { interval: 150 });
-
-sr.reveal(".about__img, .skills__data", { interval: 100 });
-sr.reveal(".about__text", { delay: 200 });
-
-sr.reveal(".work__img", { interval: 200 });
-sr.reveal(".contact__input", { interval: 150 });
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text, .section-title', {});
+sr.reveal('.home__img, .about__subtitle, .about__text', { delay: 400 });
+sr.reveal('.home__social-icon', { interval: 200 });
+sr.reveal('.skills__data, .work__item, .contact__input, .contact__info', { interval: 150 });
+sr.reveal('.project__card', { interval: 200 });
